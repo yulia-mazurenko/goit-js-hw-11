e2fa29642a9f7f7b5a1024eb38061674c60197a0
yulia-mazurenko <yulia.mazurenko1@gmail.com>
2022-11-11 23:16:31 +0200

    make infinite scroll

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
 import fetchSubjects from './fetchSubjects.js'
-// import './intersection-observer.js'
+import './scroll-top-button.js'
 
 getEl = selector => document.querySelector(selector)
 getEl('.search-form').addEventListener('submit', onFormSubmit)
-getEl('.load-more').addEventListener('click', onLoadMoreBtnClick)
+// getEl('.load-more').addEventListener('click', onLoadMoreBtnClick)
 
 
 let searchSubject=''
@@ -15,102 +15,95 @@ let pageCount = 1;
 let lightbox;
 
 function onFormSubmit(evt) {
-
+  clearMarkup()
+  
   evt.preventDefault();
   pageCount=1  
   searchSubject = evt.currentTarget.elements.searchQuery.value;
-
+if (searchSubject!=='') {
    fetchSubjects(searchSubject, pageCount).then(data => {
    renderMarkup(data)
    lightbox = new SimpleLightbox('.gallery a', {
     
-     captionsData: "alt",
+     captionsData: "author",
     captionDelay: 250,
    })
      lightbox.refresh()
 
-     makeSmoothScrolling()
+    //  makeSmoothScrolling()
      pageCount += 1
-     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
-     getEl('.load-more').classList.remove('is-hidden')
-     
+     if (data.totalHits !== 0) {
+       Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+     }
     
-   }).catch(console.log)    
+    //  getEl('.load-more').classList.remove('is-hidden')
+        
+   }).catch(console.log)  
+}
+    
 }
 
-function onLoadMoreBtnClick() {
+// function onLoadMoreBtnClick() {
   
-  searchSubject = getEl('.search-form input').value
+//   searchSubject = getEl('.search-form input').value
     
-  getEl('.load-more').classList.add('is-hidden')
+//   getEl('.load-more').classList.add('is-hidden')
   
-  fetchSubjects(searchSubject, pageCount).then(data => {
+//   fetchSubjects(searchSubject, pageCount).then(data => {
 
-    renderMarkup(data) 
+//     renderMarkup(data) 
 
-    lightbox = new SimpleLightbox('.gallery a', {
+//     lightbox = new SimpleLightbox('.gallery a', {
     
-     captionsData: "alt",
-    captionDelay: 250,
-    })
-    lightbox.refresh()
+//      captionsData: "alt",
+//     captionDelay: 250,
+//     })
+//     lightbox.refresh()
 
-    makeSmoothScrolling()
+//     makeSmoothScrolling()
       
-    if ((pageCount > data.totalHits / 40) || data.hits.length < 40) {
-      Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
-      getEl('.load-more').classList.add('is-hidden')
-      return
-    } 
+//     if ((pageCount > data.totalHits / 40) || data.hits.length < 40) {
+//       Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+//       getEl('.load-more').classList.add('is-hidden')
+//       return
+//     } 
     
-    pageCount += 1
-    getEl('.load-more').classList.remove('is-hidden')    
+//     pageCount += 1
+//     getEl('.load-more').classList.remove('is-hidden')    
     
-  }).catch(console.log)  
-}
+//   }).catch(console.log)  
+// }
 
 function renderMarkup(data) {
-  clearMarkup()
-
+  
   if (data.hits.length === 0) {
     Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
   }
 
-    const markup = data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-    return `<div class="photo-card">
-    <a href="${largeImageURL}">
-    <img class="photo-card-pic" src="${webformatURL}" alt="${tags}" width=220px height=180px loading="lazy" />
-    </a>
+  const markup = data.hits.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads, user}) => {
+        return `<div class="photo-card">
+        <a href="${largeImageURL}">
+        <img class="photo-card-pic" src="${webformatURL}" alt="${tags}" author="${user}" width=220px height=180px loading="lazy" />
+        </a>
 
-    <div class="info">
-    <p class="info-item">
-      <b>
-      Likes:
-      ${likes}
-      </b>
-    </p>
-    <p class="info-item">
-      <b>
-      Views:
-      ${views}</b>
-    </p>
-    <p class="info-item">
-      <b>
-      Comments:
-      ${comments}</b>
-    </p>
-    <p class="info-item">
-      <b>
-      Downloads:
-      ${downloads}</b>
-    </p>
+        <div class="info">
+        <p class="info-item">
+          <b>Likes:</b><span class="info-text">${likes}</span>
+        </p>
+        <p class="info-item">
+          <b>Views:</b><span class="info-text">${views}</span>
+        </p>
+        <p class="info-item">
+          <b>Comments:</b><span class="info-text">${comments}</span>
+        </p>
+        <p class="info-item">
+          <b>Downloads:</b><span class="info-text"> ${downloads}</span>
+        </p>
   </div>
-</div>`
+                </div>`
   }).join('')
 
-  getEl('.gallery').insertAdjacentHTML('beforeend', markup)
-
-   
+  getEl('.gallery').insertAdjacentHTML('beforeend', markup)   
 }
 
 function clearMarkup() {
@@ -132,5 +125,57 @@ function makeSmoothScrolling() {
 
 
 }
+
+const onEntry = (entries) => {
+  searchSubject = getEl('.search-form input').value
+  entries.forEach((entry) => {
+      
+        if(entry.isIntersecting && searchSubject!=='') {
+            
+          fetchSubjects(searchSubject, pageCount).then(data => {
+          console.log('pageCount в начале onEntry', pageCount)
+
+          renderMarkup(data) 
+
+          lightbox = new SimpleLightbox('.gallery a', {
+          
+          captionsData: "author",
+          captionDelay: 250,
+          })
+  
+         lightbox.refresh()
+
+    // makeSmoothScrolling()
+      
+            if ((pageCount > data.totalHits / 40) || data.hits.length < 40) {
+      
+                Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+                observer.unobserve(target);
+                return
+    } 
+    
+            pageCount += 1
+            
+            console.log('pageCount в конце onEntry', pageCount)
+        
+  }).catch(console.log)  
+} 
+})    
+}
+
+/*
+infinite-scroll
+*/
+
+const options = {
+  rootMargin: '150px',
+//   threshold: 1.0
+}
+
+const observer = new IntersectionObserver(onEntry, options);
+const target = document.querySelector('.sentinel')
+
+observer.observe(target);
+
 
 
---
+++ src/scroll-top-button.js
@@ -0,0 +1,17 @@
+/*
+scroll-top-button
+*/
+getEl = selector => document.querySelector(selector)
+getEl('.js-button-scroll-top').addEventListener('click', onScrollBtnClick)
+
+  window.onscroll = () => {
+  if (window.scrollY > 700) {
+    getEl('.js-button-scroll-top').classList.add('is-show')
+  } else if (window.scrollY < 700) {
+    getEl('.js-button-scroll-top').classList.remove('is-show')
+  }
+}
+
+function onScrollBtnClick() {
+    window.scrollTo(0,0)
+}
